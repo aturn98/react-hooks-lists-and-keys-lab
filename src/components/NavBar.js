70b23c809e529aa56cac05f2a043d3f18db4c220
Austin <aturn9880@gmail.com>
2021-07-12 17:58:56 -0400

    commit tree 50693cedea5a7c6a137bb7fdbac00b63041da642

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { nanoid } from "nanoid";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksList = links.map((link) => {
+    return <a key={nanoid()} href={`#${link}`}> {link} </a>
+  })
+
+  return (
+    <nav>
+      {linksList}
+    </nav>
+  );
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { nanoid } from 'nanoid';
 
 function ProjectItem({ name, about, technologies }) {
+  
+  const techSpan = technologies.map((technology) => {
+    return <span key={nanoid()}> {technology} </span>
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {techSpan}
       </div>
     </div>
   );
